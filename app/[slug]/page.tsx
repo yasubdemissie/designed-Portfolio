e5c1e2dd86bda83bebbf711afd028379ed3491bc
yasubdemissie <yasdam777@gmail.com>
2025-08-27 18:13:28 +0300

    to the final round still commit

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,13 +1,12 @@
 import { CaseStudy } from "@/components/CaseStudy";
 import { Mockup } from "@/components/Mockup";
 
-export default async function Works({
-  params,
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+interface Props {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Works({ params, searchParams }: Props) {
   const { slug } = await params;
   const { tab } = await searchParams;
 
